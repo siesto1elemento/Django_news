d6bb2310c8827b99641b27d1f2e61a326b76a462
siesto1elemento <rohitojha9720@gmail.com>
2024-02-06 23:50:32 +0530

    Fetched data from backend and presented in the templates

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def posts(request):
+    all_post = Post.objects.all()
+    
+    
+    return render(request,'home.html',{"all_post":all_post})
+    
+    
