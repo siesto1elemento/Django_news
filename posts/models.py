59ded6d9fa4f9b36c34c4d3b15fd407d2aa0aec7
siesto1elemento <rohitojha9720@gmail.com>
2024-04-12 23:29:42 +0530

    .

--- posts/models.py
+++ posts/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Post(models.Model):
     title = models.CharField(max_length=255,blank=True)
     link = models.URLField(max_length=200)
